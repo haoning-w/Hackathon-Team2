8557095b8a620e0b4a4baaf150f8976639ba8578
haoning-w <elwin.hn.wang@gmail.com>
2023-11-19 14:21:35 -0800

    Added a sample button

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,6 +1,8 @@
+import { Button } from "@mui/material";
 import DonationForm from "../features/test-donations/DonationForm";
 import TestDashboard from "../features/test-donations/TestDashboard";
 import CartOverview from "../features/test-feature/CartOverview";
+import toast from "react-hot-toast";
 
 function Test() {
   return (
@@ -8,6 +10,13 @@ function Test() {
       <p className="text-emerald-500 text-[48px] text-center">
         Test all kinds of stuff
       </p>
+      <Button
+        variant="contained"
+        sx={{ margin: "8px auto", display: "block" }}
+        onClick={() => toast.success("Some message")}
+      >
+        Make a toast
+      </Button>
       <DonationForm />
       <CartOverview />
       <TestDashboard />
