8e8382a7e3dda0b598d2c373883fb82b6173f51c
haoning-w <elwin.hn.wang@gmail.com>
2023-11-22 13:03:39 -0800

    123

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import store from "./store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Toast from "./ui/Toast";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
 
