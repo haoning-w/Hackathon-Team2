200eba1d5c6ea2ba7d01beb6f3ec3f8b777a6f00
haoning-w <elwin.hn.wang@gmail.com>
2023-11-23 14:53:46 -0800

    1

--- client/src/features/supplies/SuppliesList.jsx
+++ client/src/features/supplies/SuppliesList.jsx
@@ -3,32 +3,36 @@ import { dataToCoords } from "../../utils/helper";
 import useGetCoords from "../map/useGetCoords";
 import SupplyItem from "./SupplyItem";
 import getSuppliers from "../../services/getSuppliers";
+import useGetRequests from "../requests/useGetRequests";
 
 function transformData(originalData) {
-  return originalData.map(item => {
+  return originalData.map((item) => {
     return {
-      id: String(item.id), 
+      id: String(item.id),
       name: item.organizationName,
       address: item.address,
-      products: item.products.map(product => ({
+      products: item.products.map((product) => ({
         name: product.productName,
-        quantity: product.quantity
+        quantity: product.quantity,
       })),
-      timeStamp: new Date(item.products[0]?.createdAt).toLocaleString()
+      timeStamp: new Date(item.products[0]?.createdAt).toLocaleString(),
     };
   });
 }
 
 function SuppliesList() {
+  const { data, isLoading } = useGetRequests();
 
-  const fakeData = transformData(getSuppliers());
+  if (isLoading) return null;
+
+  const realData = transformData(data);
 
   // const { data, isLoading } = useGetCoords("4567 Lougheed Hwy");
   // if (!isLoading) console.log(dataToCoords(data));
 
   return (
     <ul>
-      {fakeData.map((item) => (
+      {realData.map((item) => (
         <SupplyItem key={item.id} supply={item} />
       ))}
     </ul>
--- client/src/ui/Requests.jsx
+++ client/src/ui/Requests.jsx
@@ -1,27 +1,32 @@
 import RequestItem from "./RequestItem";
 import getDemanders from "../services/getDemanders";
+import useGetRequests from "../features/requests/useGetRequests";
 
 function transformData(originalData) {
-  return originalData.map(item => {
+  return originalData.map((item) => {
     return {
-      id: String(item.id), 
+      id: String(item.id),
       name: item.organizationName,
       address: item.address,
-      products: item.products.map(product => ({
+      products: item.products.map((product) => ({
         name: product.productName,
-        quantity: product.quantity
+        quantity: product.quantity,
       })),
-      timeStamp: new Date(item.products[0]?.createdAt).toLocaleString()
+      timeStamp: new Date(item.products[0]?.createdAt).toLocaleString(),
     };
   });
 }
 
 function Requests() {
-  const fakeData =  transformData(getDemanders());
+  const { data, isLoading } = useGetRequests();
+
+  if (isLoading) return null;
+
+  const realData = transformData(data);
 
   return (
     <ul>
-      {fakeData.map((item) => (
+      {realData.map((item) => (
         <RequestItem key={item.id} request={item} />
       ))}
     </ul>
