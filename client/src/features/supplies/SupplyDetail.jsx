f532a3cc40c9dfc2f4323e3b3b568680758047b3
haoning-w <elwin.hn.wang@gmail.com>
2023-11-23 19:06:46 -0800

    1

--- client/src/features/supplies/SupplyDetail.jsx
+++ client/src/features/supplies/SupplyDetail.jsx
@@ -1,37 +1,33 @@
 import DeliverForm from "../../ui/DeliverForm";
 import getSuppliers from "../../services/getSuppliers";
-
-function transformData(originalData) {
-  return originalData.map(item => {
-    return {
-      id: String(item.id), 
-      name: item.organizationName,
-      address: item.address,
-      products: item.products.map(product => ({
-        name: product.productName,
-        quantity: product.quantity
-      })),
-      timeStamp: new Date(item.products[0]?.createdAt).toLocaleString()
-    };
-  });
-}
-
+import useGetSuppliers from "../map/useGetSuppliers";
+import { useParams } from "react-router-dom";
 
 function SupplyDetail() {
-  const supply = transformData(getSuppliers());;
-  const { id, name, timeStamp, address, latlng, products } = supply;
-  return ( 
+  const { data, isLoading } = useGetSuppliers();
+  const { id } = useParams();
+  if (isLoading) return null;
+  console.log(data);
+  const supply = data.find((item) => item.id === Number(id));
+  const {
+    id: supplyId,
+    organizationName,
+    timeStamp,
+    address,
+    products,
+  } = supply;
+  return (
     <div className="mt-2">
       <h1 className="text-3xl font-bold mb-2">Supplies' info</h1>
       <div className="bg-[var(--color-dark--2)] px-12 py-6 rounded-2xl">
-        <h1 className="text-[18px]">name: {name}</h1>
+        <h1 className="text-[18px]">name: {organizationName}</h1>
         <h1 className="text-[18px]">address: {address}</h1>
       </div>
       <h1 className="text-3xl font-bold mb-2 mt-8">Product info</h1>
       <div className="bg-[var(--color-dark--2)] px-12 py-6 rounded-2xl">
         {products.map((product, ind) => (
           <h1 className="text-[18px]" key={ind}>
-            {product.name}: {product.quantity}
+            {product.productName}: {product.quantity}
           </h1>
         ))}
       </div>
